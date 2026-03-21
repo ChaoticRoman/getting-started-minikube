5cbd971ae2193826ce9dd6616793313ef1a11860
Roman Pavelka <roman.pavelka.asi@gmail.com>
2026-03-21 19:45:41 +0100

    Add Azure Easy Auth integration

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,11 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 
 app = FastAPI()
 
 
 @app.get("/api/hello")
-def hello():
+def hello(request: Request):
+    user = request.headers.get("X-Ms-Client-Principal-Name")
+    if user:
+        return {"message": f"Hello, {user}!"}
     return {"message": "Hello, World!"}
